84ad2d97333ccb1b5c4f676619c96a35f9fe7d9d
bt-glv <bruno@glv.sh>
2026-08-21 16:58:22 -0300

    WIP(link): code refactor

--- link.lua
+++ link.lua
@@ -1,102 +1,77 @@
+#!/usr/bin/env lua
+
 -- This is a system/hard link farming script
 -- Declare folders/files present on the same folder
 -- as this file to be linked at the root path
-
-#!/usr/bin/env lua
+--
 package.path = ";./?.lua"
 require('sh');
 
-
 local root_path = "~/"
-local links = {
-	{".bashrc", 			"hard"},
-	{".tmux.conf", 			"hard"},
-	{".vimrc", 				"hard"},
-	{".zshrc", 				"hard"},
-	{".ssh/", 				"sys"},
-	{".config/nvim/",		"sys"},
-	{".config/alacritty/",	"sys"},
-	{".local/share/nvim/",	"sys"},
-	{".local/share/fonts/",	"sys"},
+local backup_paths = {
+	{".bashrc", 						"hard"},
+	{".config/fish/config.fish", 		"sys"},
+	{".zshrc", 							"hard"},
+	{".tmux.conf", 						"hard"},
+	{".vimrc", 							"hard"},
+	{".ssh/", 				            "sys"},
+	{".config/nvim/",		            "sys"},
+	{".config/alacritty/",	            "sys"},
+	{".local/share/fonts/",	            "sys"},
+	{".local/share/nvim/site/spell",	"sys"},
+	--{".local/share/nvim/",	"sys"},
 	--{".config/autostart/",	"sys"},
 }
 
--- ideas
--- exclude folders from a path
 
--- todo
--- [ ] adapt this file to use SH for argument handling
--- [ ] adapt SH to handle argument trees
-
-
--------------
--------------
-
--- copies home folder path to this file's path
-local function snapshot(paths, base_path)
-
-		local operation = true
-		log("\n>>\n>> snapshot START\n>>")
-
-		for i, path in ipairs(paths) do
-
-			local dirname = sh('dirname '..path).out
-			sh('mkdir -p '..dirname)
-
-			sh("cp -r "..base_path..path.." "..dirname)
-			log("item "..i)
-		end
-
-		log("\n>>\n>> snapshot END\n>>")
-end
-
-local final_considerations = {
-	messages = {},
-	print = function(self)
-		if #self == 0 then return end
-		for _, data in ipairs(self.messages) do
-			print(data)
-		end
-	end,
-}
-
-
+--- Copies every file in "backup_paths" currently present 
+--- in your sistem to a backup folder.
 local function generate_backup(paths, base_path)
+
 	log('\n>>\n>>generate_backup START\n>>\n')
 	if paths == nil or base_path == nil then
 		error('generate_backup:\nParameters cannot be nil')
 	end
 
-
-	local function check_foldername(name, index)
+	---@param name string Desired dir name
+	---@param index integer? Recursive index - used to differentiate backups on the same machine
+	--- Creates the backup folder's name, checks if a folder of that name exists;
+	--- if not, it returns the name, if it does, the function calls itself recursively
+	local function define_dir_name(name, index)
 		if index == nil then index = 0 end
 
 		local new_name = name..'-'..index
 		if sh('ls | grep -Po "^'..new_name..'$"').status ~= 0 then
 			return new_name
 		end
-		return check_foldername(name, index+1)
+
+		return define_dir_name(name, index+1)
 	end
 
 
-	local backup_folder_name = check_foldername('backup.'..sh('echo "$HOSTNAME@$USER"').out)
-	sh('mkdir '..backup_folder_name)
+	local backup_dir_prefix = string.format("backup.%s", sh('echo "$HOSTNAME@$USER"').out)
+	local backup_dir_name   = define_dir_name(backup_dir_prefix)
+
+	sh('mkdir '..backup_dir_name)
 
 	local path_source_full
 	for _, path in ipairs(paths) do
+
 		path_source_full = base_path..path[1]
 
-		sh('mkdir -p $(dirname '..backup_folder_name..'/'..path[1]..')')
-
+		-- TODO: replace all concatenation with ".." with string.format for better readability
+		sh('mkdir -p $(dirname '..backup_dir_name..'/'..path[1]..')')
 		local check_syslink = sh('if [ -L "$(echo '..base_path..')'..path[1]..'" ]; then echo "t";  fi').out
+
 		log("check_syslink value: ["..check_syslink..']  type:['..type(check_syslink)..']')
 
-		if check_syslink  ~= "t" then
-			sh('cp -r '..path_source_full.." "..backup_folder_name.."/"..path[1])
+		local not_a_system_link = (check_syslink ~= "t")
+		if not_a_system_link then
+			sh('cp -r '..path_source_full.." "..backup_dir_name.."/"..path[1])
 		else
 			log('>> target path is a system link\n>> link path will be followed and copied')
 			local syslink_path = sh('readlink '..path_source_full).out
-			sh('cp -r '..syslink_path.." "..backup_folder_name.."/"..path[1])
+			sh('cp -r '..syslink_path.." "..backup_dir_name.."/"..path[1])
 		end
 
 	end
@@ -104,35 +79,13 @@ local function generate_backup(paths, base_path)
 	log('\n>>\n>> generate_backup END\n>>\n')
 end
 
-
--- TODO:
--- [ ] test this function
+-- TODO: add a way to select a backup folder to create links from it
 --
--- get list of all files and folder
--- itarate thoough each of them
--- create a hardlink for file
--- run funciton again on folder
---
-local function everything_in_folder_hardlink(base_path)
-	local res = sh([[ls -la ]]..base_path..[[| grep -Po "[^ /]+$]])
-	local entries = {}
+---@param links table Contains a list sets of "path" and  "link type"
+---@param root string Home folder path
+---Creates hard or system links to all paths defined in "links"
+local function link_bakcups(links, root)
 
-	for line in res.out:gmatch("([^\n]*)\n?") do
-        if line ~= "" then
-            table.insert(entries, line)
-        end
-    end
-
-	for _, data in ipairs(entries) do
-		if sh("ln "..data.." "..base_path..data).status == 1 then
-			everything_in_folder_hardlink(base_path..data.."/")
-		end
-	end
-
-end
-
-
-local function create_links(links, root)
 	log('\n>>\n>> create_links START\n>>\n')
 	generate_backup(links, root)
 	for i, path in ipairs(links) do
@@ -148,9 +101,12 @@ local function create_links(links, root)
 	end
 
 	log('\n>>\n>> create_links END\n>>\n')
+
 end
 
 
+-- TODO: review this function
+-- this needs to remove all system and hardlinks
 local function remove_links(links)
 	local operation = ""
 	for i, file in ipairs(links) do
@@ -161,9 +117,18 @@ end
 
 
 actions = {
-	backup = function() sh_q_enable_logs(); generate_backup	(links, root_path) 	end,
-	create = function() sh_q_enable_logs(); create_links	(links, root_path) 	end,
-	remove = function() sh_q_enable_logs(); remove_links	(links) 			end
+	backup = function()
+		sh_q_enable_logs()
+		generate_backup	(backup_paths, root_path)
+	end,
+	create = function()
+		sh_q_enable_logs()
+		link_bakcups (backup_paths, root_path)
+	end,
+	remove = function()
+		sh_q_enable_logs()
+		remove_links(backup_paths)
+	end
 }
 
 if actions[arg[1]] ~= null then
